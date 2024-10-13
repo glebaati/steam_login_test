8b3e33b0a01ce2f7872df8e8adbb7de9ac99835c
glebaati <gkramorov25@gmail.com>
2024-10-13 14:56:43 +0300

    Explicity wait adding

--- test_1.py
+++ test_1.py
@@ -1,32 +1,40 @@
 import pytest
 from selenium import webdriver
-from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
-from selenium.webdriver.common.keys import Keys
 import random
 import string
+STEAM_URL = "https://store.steampowered.com/"
+TIMEOUT = 10
+
+LOGIN_BUTTON_XPATH = "//a[contains(text(),'войти')]"
+LOGIN_FIELD_XPATH = "//input[not(@type='password')]"
+PASSWORD_FIELD_XPATH = "//input[contains(@type, 'password')]"
+SUBMIT_BUTTON_XPATH = "//button[contains(@type, 'submit')]"
+ERROR_MESSAGE_XPATH = "//form//div[5]"
+LOADING_SIGN_XPATH = "//button[@disabled]"
 @pytest.fixture()
 def driver():
     driver = webdriver.Chrome()
     driver.maximize_window()
+    driver.get(STEAM_URL)
     yield driver
 
 def random_string(length):
     letters = string.ascii_letters
-    return ''.join(random.choice(letters) for i in range(length))
+    return ''.join(random.choice(letters) for _ in range(length))
 
 def test_steam_login(driver):
-    driver.get('https://store.steampowered.com/')
-    driver.find_element(By.XPATH, "//a[contains(text(),'войти')]").click()
-    WebDriverWait(driver, 10).until(
-        EC.presence_of_element_located((By.XPATH, "//input[contains(@type, 'text') and contains(@class, '_2GBWeup')]")))
-    driver.find_element(By.XPATH, "//input[contains(@type, 'text') and contains(@class, '_2GBWeup')]").send_keys(random_string(10))
-    driver.find_element(By.XPATH, "//input[contains(@type, 'password')]").send_keys(random_string(12))
-    driver.find_element(By.XPATH, "//button[contains(@type, 'submit')]").click()
-    WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.XPATH, "//button[@disabled]")))
-    WebDriverWait(driver, 10).until(EC.invisibility_of_element_located((By.XPATH, "//button[@disabled]")))
-    error_message = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.XPATH, "//div[contains(text(), 'Пожалуйста, проверьте')]")))
-    assert "Пожалуйста, проверьте" in error_message.text
+    WebDriverWait(driver, TIMEOUT).until(EC.element_to_be_clickable((By.XPATH, LOGIN_BUTTON_XPATH))).click()
+    WebDriverWait(driver, TIMEOUT).until(EC.element_to_be_clickable((By.XPATH, LOGIN_FIELD_XPATH))).send_keys(random_string(10))
+    WebDriverWait(driver, TIMEOUT).until(EC.element_to_be_clickable((By.XPATH, PASSWORD_FIELD_XPATH))).send_keys(random_string(10))
+    WebDriverWait(driver, TIMEOUT).until(EC.element_to_be_clickable((By.XPATH, SUBMIT_BUTTON_XPATH))).click()
+    WebDriverWait(driver, TIMEOUT).until(EC.presence_of_element_located((By.XPATH, LOADING_SIGN_XPATH)))
+    WebDriverWait(driver, TIMEOUT).until(EC.invisibility_of_element_located((By.XPATH, LOADING_SIGN_XPATH)))
+    error_message = WebDriverWait(driver, TIMEOUT).until(EC.presence_of_element_located((By.XPATH, ERROR_MESSAGE_XPATH)))
+    expected_message = "Пожалуйста, проверьте"
+    actual_message = error_message.text
+    assert expected_message in actual_message, (f"Failed: Expected message: '{expected_message}', "
+        f"but got: '{actual_message}'")
 
